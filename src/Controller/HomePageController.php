267a37b7f7b941fcdc555e4fb6c661c1f0274778
Web1985 <webwork1985@gmail.com>
2024-12-04 00:13:45 +0100

    get request

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -1,16 +1,22 @@
 <?php
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomePageController extends AbstractController {
     #[Route(path:"/", name:"home")]
-    public function index(): Response {
+    public function index(Request $request): Response {
+        $get_name = [];
+        if( $name = $request ->query->get("name") ) {
+            $get_name = htmlspecialchars($name);
+        };
         //$title = $this->getDoctrine()->getRepository("test")->findOneBy(array(
         $title = "Home page";
         return $this->render("home.html.twig", [
             "title"=> $title,
+            "get_name"=> $get_name
 
         ]);
     }
