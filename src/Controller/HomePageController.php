874eb3d71f307a6f247b876d28cabad5bac7f78a
Web1985 <webwork1985@gmail.com>
2024-12-03 23:44:48 +0100

    home page

---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomePageController extends AbstractController {
+    #[Route("/")]
+    public function index(): Response {
+        //$title = $this->getDoctrine()->getRepository("test")->findOneBy(array(
+        $title = "Home page";
+        return $this->render("home.html.twig", [
+            "title"=> $title,
+
+        ]);
+    }
+}
