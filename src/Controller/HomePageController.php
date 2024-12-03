687602f502da473f0809654425adef4c3114dd68
Web1985 <webwork1985@gmail.com>
2024-12-04 00:04:39 +0100

    conference controller

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -5,7 +5,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomePageController extends AbstractController {
-    #[Route("/")]
+    #[Route(path:"/", name:"home")]
     public function index(): Response {
         //$title = $this->getDoctrine()->getRepository("test")->findOneBy(array(
         $title = "Home page";
