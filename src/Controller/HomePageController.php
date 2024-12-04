1a549376da33c97edec9a7fc54df785d69016782
Web1985 <webwork1985@gmail.com>
2024-12-04 14:30:26 +0100

    4

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomePageController extends AbstractController {
     #[Route(path:"/", name:"home")]
     public function index(Request $request): Response {
-        $get_name = [];
+        $get_name = '';
         if( $name = $request ->query->get("name") ) {
             $get_name = htmlspecialchars($name);
         };
@@ -16,8 +16,7 @@ class HomePageController extends AbstractController {
         $title = "Home page";
         return $this->render("home.html.twig", [
             "title"=> $title,
-            "get_name"=> $get_name
-
+            "get_name"=> $get_name,
         ]);
     }
 }
